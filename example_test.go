d439e8462bc1cab8398f6e736badd19deb60506a
Marc-Antoine Ruel <maruel@gmail.com>
2025-08-11 18:05:37 -0400

    Add example
    Update genai post genaitools removal.

---
+++ example_test.go
@@ -0,0 +1,51 @@
+// Copyright 2025 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package genaitools_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/maruel/genai"
+	"github.com/maruel/genai/adapters"
+	"github.com/maruel/genai/providers/anthropic"
+	"github.com/maruel/genaitools"
+	"github.com/maruel/roundtrippers"
+)
+
+func Example_genSyncWithToolCallLoop_with_custom_HTTP_Header() {
+	// Modified version of the example in package adapters, with a custom header.
+	//
+	// As of June 2025, interleaved thinking can be enabled with a custom header.
+	// https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#interleaved-thinking
+	wrapper := func(h http.RoundTripper) http.RoundTripper {
+		return &roundtrippers.Header{
+			Transport: h,
+			Header:    http.Header{"anthropic-beta": []string{"interleaved-thinking-2025-05-14"}},
+		}
+	}
+	c, err := anthropic.New(&genai.OptionsProvider{Model: "claude-sonnet-4-20250514"}, wrapper)
+	if err != nil {
+		log.Fatal(err)
+	}
+	msgs := genai.Messages{
+		genai.NewTextMessage("What is 3214 + 5632? Leverage the tool available to you to tell me the answer. Do not explain. Be terse. Include only the answer."),
+	}
+	opts := genai.OptionsText{
+		Tools: []genai.ToolDef{genaitools.Arithmetic},
+		// Force the LLM to do a tool call first.
+		ToolCallRequest: genai.ToolCallRequired,
+	}
+	newMsgs, _, err := adapters.GenSyncWithToolCallLoop(context.Background(), c, msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", newMsgs[len(newMsgs)-1].AsText())
+	// Remove this comment line to run the example.
+	// Output:
+	// 8846
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/maruel/genaitools
 
 go 1.24.4
 
-require github.com/maruel/genai v0.0.0-20250811193417-4ac87fbea34f
+require (
+	github.com/maruel/genai v0.0.0-20250811220235-aed5dc34ed45
+	github.com/maruel/roundtrippers v0.4.2
+)
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
@@ -12,8 +15,8 @@ require (
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/maruel/httpjson v0.5.0 // indirect
-	github.com/maruel/roundtrippers v0.4.2 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	gopkg.in/dnaeon/go-vcr.v4 v4.0.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
